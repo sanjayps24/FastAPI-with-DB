b78b8db0c1a0eb0cbbb932fa9b63b3acc80321d9
Sanjayps24 <sanjayps2407@gmail.com>
2026-02-12 12:56:58 +0530

    Implemented the DB with gpt frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user_routes import router as user_router
 from routes.ai_response_routes import router as ai_response_router
 from db import get_db,DATABASE_URL
@@ -7,6 +8,14 @@ import os
 from models import Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(ai_response_router)
